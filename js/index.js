652c138eec0d7f12feb736caa7fcb158bd7dcd87
orsoli <fljcoli@gmail.com>
2024-08-20 15:44:21 +0200

    added DOM elements

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
-console.log("js ok");
+/*
+    Given an array containing a list of five images. 
+    Create a carousel with attached photos.
+    You can also use different images if you want, the important thing is the logic!
+*/
+
+// Milestones 1
+  //Create the static elements in DOM.
+
+
+
+
