0b2d1a3436b0ea0ab3a0aa3d2cef0e4dcbfde26d
orsoli <fljcoli@gmail.com>
2024-08-20 16:10:48 +0200

    added preparation phase

--- js/index.js
+++ js/index.js
@@ -7,6 +7,16 @@
 // Milestones 1
   //Create the static elements in DOM.
 
+// --- Preparation phase
+// Retrieve the interested elements in DOM
+const chvronUpElement = document.getElementById('chevron-up')
+const chvronDownElement = document.getElementById('chevron-down')
+const images = document.querySelectorAll('.carousel img')
 
+// Create the current variable index
+let currentActiveIndex = 0;
+// Add the active class in current img
+const image = images[currentActiveIndex]
+image.classList.add('active')
 
-
+// --- Processing phase
